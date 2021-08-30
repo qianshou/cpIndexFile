c8a9fe93d8b9a33523ba078bc1f5b7876e57e42e
qianshou <799631071@qq.com>
2021-08-30 22:28:37 +0800

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const process = require('process');
+const path = require('path');
+
+const fileName = process.argv[2];
+const fromIndex = process.argv[3];
+const toIndex = process.argv[4];
+
+const fileSuffix = path.extname(fileName);
+const filePrefix = fileName.replace(fileSuffix, '');
+
+for(var i=fromIndex; i<=toIndex; i++){
+    var fileNameNew = filePrefix + '_' + i + fileSuffix;
+    fs.copyFileSync(fileName, fileNameNew);
+}
